f2496df14452233aafd5892e3a09e860b7765883
Paul Orha <orha.paul@gmail.com>
2017-11-13 16:47:26 +0200

    Finished getters and setters

--- controller/activityController.py
+++ controller/activityController.py
@@ -5,3 +5,33 @@ class Activity:
         self.__date = date
         self.__time = time
         self.__description = description
+
+    def setActivityId(self, activityId):
+        self.__activityId = activityId
+
+    def getActivityId(self):
+        return self.__activityId
+
+    def setPersonIds(self, personIds):
+        self.__personIds = personIds
+
+    def getPersonIds(self):
+        return self.__personIds
+
+    def setDate(self, date):
+        self.__date = date
+
+    def getDate(self):
+        return self.__date
+
+    def setTime(self, time):
+        self.__time = time
+
+    def getTime(self):
+        return self.__time
+
+    def setDescription(self, description):
+        self.__description = description
+
+    def getDescription(self):
+        return self.__description
